a3e9e3f760803ea7f42f3f84c7da24fd4b9c8b4b
Dragomir-0 <39728866+Dragomir-0@users.noreply.github.com>
2021-09-10 12:54:50 +0200

    Add project files.

---
+++ BlazorFirstBlood/Client/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFirstBlood.Client.Pages
+{
+    public partial class Counter : ComponentBase
+    {
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            currentCount++;
+        }
+    }
+}
---
+++ BlazorFirstBlood/Client/Pages/CounterRazor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFirstBlood.Client.Pages
+{
+    public partial class CounterRazor : ComponentBase
+    {
+        
+    }
+}
